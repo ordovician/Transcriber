7d4b315b7e1e38888c30c42b3d19adbf01ed7aa1
Erik Engheim <erik.engheim@mac.com>
2021-06-24 17:09:59 +0200

    Initial Commit

---
+++ Transcriber/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Transcriber
+//
+//  Created by Erik Engheim on 24/06/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
